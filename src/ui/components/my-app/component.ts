51554a940e456518f59f57371c1cc68e097b758f
Daniel Brain <dbrain@paypal.com>
2017-04-06 01:07:55 -0700

    Add glimmer/xcomponent example

--- src/ui/components/my-app/component.ts
+++ src/ui/components/my-app/component.ts
@@ -1,5 +1,18 @@
-import Component from "@glimmer/component";
+import Component, { tracked } from "@glimmer/component";
 
 export default class MyApp extends Component {
 
+    @tracked loggedIn: boolean = false;
+    @tracked email: string = '';
+
+    constructor() {
+        super();
+
+        this.prefilledEmail = 'foo@bar.com';
+
+        this.onLogin = (email) => {
+            this.loggedIn = true;
+            this.email = email;
+        };
+    }
 }
