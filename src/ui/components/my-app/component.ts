1a8752bbef90091bee881f82a3798822c1b4d08d
Daniel Brain <dbrain@paypal.com>
2017-04-06 10:33:01 -0700

    Use action to preserve this in callback

--- src/ui/components/my-app/component.ts
+++ src/ui/components/my-app/component.ts
@@ -2,17 +2,13 @@ import Component, { tracked } from "@glimmer/component";
 
 export default class MyApp extends Component {
 
-    @tracked loggedIn: boolean = false;
-    @tracked email: string = '';
+    @tracked loggedIn = false;
+    @tracked email = '';
 
-    constructor() {
-        super();
+    @tracked prefilledEmail = 'foo@bar.com';
 
-        this.prefilledEmail = 'foo@bar.com';
-
-        this.onLogin = (email) => {
-            this.loggedIn = true;
-            this.email = email;
-        };
+    onLogin(email) {
+        this.loggedIn = true;
+        this.email = email;
     }
 }
